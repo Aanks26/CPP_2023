61b79b479f9c99d2baecddcd4988a855d335ef52
Aanks26 <125660577+Aanks26@users.noreply.github.com>
2023-04-06 20:53:39 +0800

    Update M02.cpp

--- M02.cpp
+++ M02.cpp
@@ -2,17 +2,19 @@
 #include <math.h>
 using namespace std;
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    int A, N;
     float x, S=0;
     double const pi=3.14159265359;
+    int pl(int A, int N){
+    x=tan(pi/N);
+    S=(A*A*N)/(4*x);
+    cout<<"Площадь "<<N<<"-угольника со стороной "<<A<<" равна "<<S<<endl;
+    }
+    int main() {
+    setlocale(LC_ALL, "Russian");
+    int A, N;
     printf("Введите сторону A: ");
     scanf("%d", &A);
     printf("Введите число сторон N: ");
     scanf("%d", &N);
-    x=tan(pi/N);
-    S=(A*A*N)/(4*x);
-    cout<<"Площадь "<<N<<"-угольника со стороной "<<A<<" равна "<<S<<endl;
-    return 0;
+    pl(A, N);
 }
