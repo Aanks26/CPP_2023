2a79be832188a3156bf90946adcba474dc3477be
Aanks26 <125660577+Aanks26@users.noreply.github.com>
2023-04-11 21:55:45 +0800

    Create S21.cpp

---
+++ S21.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+    int main() {
+    setlocale(LC_ALL, "Russian");
+    int A,B;
+    printf("Введите число A: ");
+    scanf("%d", &A);
+    printf("Введите число B (оно должно быть больше числа A): ");
+    scanf("%d", &B);
+    ofstream out;
+    out.open("hui.txt");
+    for (int i=A; i<=B; ++i){
+    out<<i*3<<endl;
+    }
+    return 0;
+    out.close();
+    }
