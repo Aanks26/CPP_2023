fec91b4d4c60a6c2e9d24b31cda750938d60b120
Aanks26 <125660577+Aanks26@users.noreply.github.com>
2023-04-21 18:11:35 +0800

    Create S72.cpp

---
+++ S72.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class CreditCard{
+public:
+    int Number;
+    float Balance;
+
+    CreditCard(int n, int b);
+    int Put(int v);
+    int Take(int v);
+    int show_balance();
+};
+
+CreditCard::CreditCard(int n, int b){
+    Number=1234;    Balance=0;
+    Number=n;   Balance=b;
+}
+
+CreditCard::Put(int v){
+    Balance=Balance+v;
+    cout<<"Операция пополнения проведена. Текущий баланс карты: "<<Balance<<endl;
+}
+
+CreditCard::Take(int v){
+    Balance=Balance-v;
+    cout<<"Операция снятия проведена. Текущий баланс карты: "<<Balance<<endl;
+}
+
+CreditCard::show_balance(){
+    cout<<"Текущая карта: "<<Number<<endl;
+    cout<<"Текущий баланс: "<<Balance<<endl;
+}
+
+int Transfer(CreditCard C1, CreditCard C2, int V){
+        if ((C1.Balance <= 0)||(C1.Balance < V)){
+        cout<<"Недостаточно средств для осуществления перевода"<<endl;
+    }
+    else if (C1.Number != C2.Number){
+        int a, b;
+        a=C1.Balance-V;
+        b=C2.Balance+V;
+        cout<<"Текущий баланс карты-отправителя: "<<a<<endl;
+        cout<<"Текущий баланс карты-получателя: "<<b<<endl;}
+    else{
+        cout<<"Невозможно осуществить перевод: выбрана одна и та же карта"<<endl;
+    }
+
+};
+    int main() {
+        setlocale(LC_ALL, "Russian");
+        CreditCard cc1 (5032, 500);
+        CreditCard cc2 (2305, 500);
+        Transfer (cc1, cc2, 300);
+}
