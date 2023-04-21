5f11c2dde408ab5fa1f085164fd4b5270a970668
Aanks26 <125660577+Aanks26@users.noreply.github.com>
2023-04-21 17:43:14 +0800

    Create S71.cpp

---
+++ S71.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class CreditCard{
+private:
+    int Number;
+    float Balance;
+
+public:
+    CreditCard(int n, int b);
+    int Put(int v);
+    int Take(int v);
+    int show_balance();
+};
+
+CreditCard::CreditCard(int n, int b){
+    Number=1234;    Balance=0;
+    Number=n;   Balance=b;
+}
+
+CreditCard::Put(int v){
+    Balance=Balance+v;
+    cout<<"Операция пополнения проведена. Текущий баланс карты: "<<Balance<<endl;
+}
+
+CreditCard::Take(int v){
+    Balance=Balance-v;
+    cout<<"Операция снятия проведена. Текущий баланс карты: "<<Balance<<endl;
+}
+
+CreditCard::show_balance(){
+    cout<<"Текущая карта: "<<Number<<endl;
+    cout<<"Текущий баланс: "<<Balance<<endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    CreditCard cc1(5032, 200);
+    CreditCard * note;
+    note = &cc1;
+    note->show_balance();
+    note->Put(20);
+    note->Take(50);
+}
