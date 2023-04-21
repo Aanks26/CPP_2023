e2a84f70dff10c4a38eff81a7d4b7632520a2f7a
Aanks26 <125660577+Aanks26@users.noreply.github.com>
2023-04-21 17:27:54 +0800

    Update S21.cpp

--- S21.cpp
+++ S21.cpp
@@ -2,21 +2,19 @@
 #include <fstream>
 using namespace std;
 
-    int main() {
+int main() {
     setlocale(LC_ALL, "Russian");
     int A,B;
     printf("Введите число A: ");
     scanf("%d", &A);
     printf("Введите число B (оно должно быть больше числа A): ");
     scanf("%d", &B);
-    if (B<A){
-        printf("Число A больше числа B");
-    }
+        if (B<A){
+            printf("Число A больше числа B");}
     ofstream out;
     out.open("output.txt");
-    for (int i=A; i<=B; ++i){
-    out<<i*3<<endl;
-    }
+        for (int i=A; i<=B; ++i){
+            out<<i*3<<endl;}
     return 0;
     out.close();
-    }
+}
