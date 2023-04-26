db95105dfdfcb8f467ac979f8542787b3017636b
Aanks26 <125660577+Aanks26@users.noreply.github.com>
2023-04-26 15:04:09 +0800

    Update S01.cpp

--- S01.cpp
+++ S01.cpp
@@ -7,17 +7,10 @@ int main(){
     float b=3.1;
     char c='M';
     bool d=false;
-    cout<<"a="<<a<<endl;
-    cout<<"b="<<b<<endl;
-    cout<<"c="<<c<<endl;
-    cout<<"d="<<d<<endl;
-    float s=a+b;
-    float v=a-b;
-    float y=a*b;
-    float dl=a/b;
-    cout<<"Сумма равна: "<<s<<endl;
-    cout<<"Разность равна: "<<v<<endl;
-    cout<<"Результат умножения равен: "<<y<<endl;
-    cout<<"Результат деления равен: "<<dl<<endl;
+    cout<<"a="<<a<<endl<<"b="<<b<<endl<<"c="<<c<<endl<<"d="<<d<<endl;
+    cout<<"Сумма равна: "<<a+b<<endl;
+    cout<<"Разность равна: "<<a-b<<endl;
+    cout<<"Результат умножения равен: "<<a*b<<endl;
+    cout<<"Результат деления равен: "<<a/b<<endl;
     return 0;
 }
