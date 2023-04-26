1f69bbd9cc24e4e62e7fcf29fc485fcd819269f2
Aanks26 <125660577+Aanks26@users.noreply.github.com>
2023-04-26 14:22:14 +0800

    Create S02.cpp

---
+++ S02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    int a=10, b=3;
+    cout <<"Результат еления: " <<a/b<<endl;
+    cout<<"Остаток от деления: "<<a%b<<endl;
+    return 0;
+}
